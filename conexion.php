55483a8c048bb1c609b103ad84962608f1541c07
Alexis2688 <140521402+Alexis2688@users.noreply.github.com>
2023-11-09 23:49:40 -0300

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,23 @@
+<?php
+/*
+$host = "172.190.195.181";
+$user = "Dave44";
+$pass = "Dave3747";
+$db = "FirstDB";
+*/
+
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "restaurante";
+
+$conexion = mysqli_connect($host,$user,$pass,$db);
+/*
+if ($conexion) {
+    echo "hay conexion";
+}
+
+*/
+
+?>
